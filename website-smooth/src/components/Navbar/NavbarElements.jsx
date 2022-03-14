e4f293a8214397ddb0837ccd2a17a7e6a7507156
Filipe Nogueira Silveira <fnosilveira@gmail.com>
2022-03-14 00:18:59 -0300

    UPDATE
    Add buttons, style, NavLink

--- website-smooth/src/components/Navbar/NavbarElements.jsx
+++ website-smooth/src/components/Navbar/NavbarElements.jsx
@@ -26,7 +26,7 @@
     z-index: 1;
     width: 100%;
     padding: 0 24px;
-    max-width: 1100px;
+    
 
     }
 
@@ -65,7 +65,7 @@
     list-style: none;
     text-align: center;
     margin-right: -22px;
-
+    margin-top: 60px;
     @media screen and (max-width: 768px) {
         display: none;
     }
@@ -88,5 +88,36 @@
         border-bottom: 3px solid #01bf71;
     }
  `;
+
+export const NavBtn = styled.nav`
+    display: flex;
+    align-items: center;
+
+    @media screen and (max-width: 768px){
+       display: none;
+    }
+
+`;
+
+export const NavBtnLink = styled(LinkR)`
+    border-radius: 50px;
+    background: #01bf71;
+    white-space: nowrap;
+    padding: 10px 22px;
+    color: #010606;
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover{
+      transition: all 0.2s ease-in-out;
+      background: #fff;
+      color: #0100606;
+    }
+`;
+
 
 
---
+++ website-smooth/src/components/Sidebar/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SidebarBtnWrap, SidebarRoute  } from './SidebarElement';
+
+const Sidebar = () => {
+  return (
+    <SidebarContainer>
+        <Icon>
+            <CloseIcon />
+        </Icon>
+        <SidebarWrapper>
+          <SidebarMenu>
+            <SidebarLink to="about">
+              Sobre
+            </SidebarLink>
+            <SidebarLink to="discover">
+              Discover
+            </SidebarLink>
+            <SidebarLink to="services">
+              Services
+            </SidebarLink>
+            <SidebarLink to="signup">
+              Sign Up
+            </SidebarLink>
+          </SidebarMenu>
+          <SidebarBtnWrap>
+            <SidebarRoute to="/signin">Sign Up</SidebarRoute>
+          </SidebarBtnWrap>
+        </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
+
